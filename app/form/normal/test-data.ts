149eb77aeca437265a77cdd7f07641cfeaa5e906
Joel <iamjoel007@gmail.com>
2023-09-13 15:29:46 +0800

    feat: form edit page

---
+++ app/form/normal/test-data.ts
@@ -0,0 +1,15 @@
+const data = {
+  "content": [
+    {
+      "type": "Button",
+      "props":
+        {
+          "text":"按钮",
+          "size":"sm",
+          "id":"Button-1694444005536"
+        }
+    }
+  ],
+  "root":{"title":""}
+}
+export default data
